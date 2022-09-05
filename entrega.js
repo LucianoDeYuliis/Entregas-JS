2ec4934f7ffca4446fb3426f5d62ec84e5326e56
Luciano De Yuliis <lucianodeyuliis@hotmail.com>
2022-09-05 20:49:12 -0300

    Agregar nueva entrega

--- entrega.js
+++ entrega.js
@@ -1,11 +1,28 @@
+ //const dolarBlue = 297;
+ //const fondos = 100000
+ //let cantidad = prompt("Ingresar cantidad a comprar de usd")
 
-let num = Number(prompt("Elegir numero")); 
+ //if (dolarBlue * cantidad <= fondos ) {
+   // console.log("Puede comprar")
+ //} else {
+   // console.log("No puede comprar")
+ //}
 
 
-function bucleFor(num) {
-    for(let i=0; i<=num; i+=4) {
-    console.log(i);
+
+
+ function Compra(){
+    let dolarblue = document.getElementById("dolarblue").value;
+    let cantidad = document.getElementById("cantidad").value;
+    let fondos = document.getElementById("fondos").value;
+
+    alert("Precio del usd blue: " + dolarblue);
+    alert("cantidad de usd blue a comprar: " + cantidad);
+    alert("fondos: " + fondos);
+
+    if (dolarblue * cantidad <= fondos ) {
+        alert("Puede comprar");
+    }else{
+        alert("No puede comprar");
+    }
  }
-}
-
-bucleFor(num);
