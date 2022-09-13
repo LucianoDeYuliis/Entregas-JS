ad54b20c33a77a099efc399c295fe712ce4f9390
Luciano De Yuliis <lucianodeyuliis@hotmail.com>
2022-09-12 21:37:24 -0300

    Entrega Array

---
+++ Entrega Array/main.js
@@ -0,0 +1,19 @@
+let alumnos = ['Luciano' , 'Roberto'];
+
+
+function AgregarAlumnos(){
+    
+    alumnos.push = document.getElementById("button").value;
+    
+    console.log(alumnos);
+}
+
+function EliminarAlumnos(){
+    alumnos.pop = document.getElementById("button").value;
+    console.log(alumnos);
+     
+}
+
+
+
+console.log(alumnos);
